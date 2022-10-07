25fb0eb4c9d5e62f1cb643768a743abe233e8b57
EstherWangui <esther.nganga@student.moringaschool.com>
2022-10-07 19:38:08 +0300

    committed changes made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-//import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
+function Nameform({formSubmitted, namelength}) {
+    const[newName, setnewName] = useState({"id":namelength+=1})
+    console.log(namelength)
+    function handleInput(e){
+        let value = e.target.value
+        let name = e.target.name
+        console.log(`value is ${value}`)
+        console.log(`name is ${name}`)
+        // let data = {[name]: value}
+        setnewName({...newName,[name]: value})
+
+
+    }
+    function handleAddName(e){
+      e.preventDefault()
+      console.log(newName)
+      fetch("http://localhost:3000/todos",{
+        // method, headers, body
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newName)
+      })
+      .then((res)=> res.json())
+      .then((data)=>
+        console.log(data)
+      )
+      formSubmitted()
+
+    }
+
+    console.log(newName)
   return (
     <>
+      <form onSubmit={handleAddName}>
+        <label>
+         Name:
+          <input onBlur={handleInput} type="text" name="name" />
+        </label>
+        <label>
+          years:
+          <input onBlur={handleInput} type="text" name="years" />
+        </label>
+        <label>
+          Dateofbirth:
+          <input onBlur={handleInput} type="text" name="Dteofbirth" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </>
   );
 }
-
-export default App;
+export default Nameform;
